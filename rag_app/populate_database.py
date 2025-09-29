e77272f0c00d806eaee514251c157a522ea459bb
Yudha Pratama <51451256+firstyudha@users.noreply.github.com>
2025-09-29 14:06:44 +0700

    Update populate_database.py

--- rag_app/populate_database.py
+++ rag_app/populate_database.py
@@ -4,7 +4,7 @@ import shutil
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
-from rag_app.get_embedding_function import get_embedding_function
+from get_embedding_function import get_embedding_function
 from langchain_chroma import Chroma
 
 
